885af370a39ae556835199b26335e9d3be359566
YKP9 <pillaiyashwanth.9@gmail.com>
2024-09-03 14:24:31 +0530

    Setup Basic Structure of Client Side - 4

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { Button } from 'antd';
+import './styleSheets/alignments.css'
+import './styleSheets/custom-components.css'
+import './styleSheets/form-elements.css'
+import './styleSheets/layout.css'
+import './styleSheets/text-elements.css'
+import './styleSheets/theme.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+<div>
+  <Button type='primary'>HELLO MINEWALLET !!!</Button>
+</div>
+      
+    </>
+  )
+}
+
+export default App
