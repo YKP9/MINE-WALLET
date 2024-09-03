38d9ec2dddcd5157364ab4d4452e40a420a7bf33
YKP9 <pillaiyashwanth.9@gmail.com>
2024-09-03 15:15:12 +0530

    Add font, Basic Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,20 @@
-import { Button } from 'antd';
-import './styleSheets/alignments.css'
-import './styleSheets/custom-components.css'
-import './styleSheets/form-elements.css'
-import './styleSheets/layout.css'
-import './styleSheets/text-elements.css'
-import './styleSheets/theme.css'
+
+import "./styleSheets/alignments.css"
+import "./styleSheets/custom-components.css"
+import "./styleSheets/form-elements.css"
+import "./styleSheets/layout.css"
+import "./styleSheets/text-elements.css"
+import "./styleSheets/theme.css"
+
 
 
 function App() {
   
 
   return (
-    <>
-<div>
-  <Button type='primary'>HELLO MINEWALLET !!!</Button>
-</div>
-      
-    </>
+    <div>
+
+    </div>
   )
 }
 
