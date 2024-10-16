e9b6df1a17ec5b0a43a05adf4cd35b7a5da3412c
YKP9 <pillaiyashwanth.9@gmail.com>
2024-10-16 17:08:40 +0530

    Axios Instance Configured

--- client/src/apiCalls/index.jsx
+++ client/src/apiCalls/index.jsx
@@ -1,7 +1,25 @@
-import axios from 'axios'
+// import axios from 'axios'
+
+// export const axiosInstance = axios.create({
+//     headers : {
+//         "authorization" : `Bearer ${localStorage.getItem('token')}`
+//     }
+// })
+
+import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    headers : {
-        "authorization" : `Bearer ${localStorage.getItem('token')}`
-    }
-})
+  baseURL: import.meta.env.VITE_API_URL, // Base URL from environment variables
+  withCredentials: true, // Allows sending cookies across domains
+});
+
+// Interceptor to set the Authorization header for each request
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
