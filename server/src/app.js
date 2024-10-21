ac39299ddcdc557767bd9cc609908a490e00e2a3
YKP9 <pillaiyashwanth.9@gmail.com>
2024-10-21 13:57:49 +0530

    Temp

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: [process.env.CORS_ORIGIN,'https://localhost:5173'],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
