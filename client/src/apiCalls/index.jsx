3662c624e8d9326870e47b8f092f1bd89b9a95f0
YKP9 <pillaiyashwanth.9@gmail.com>
2024-10-21 13:45:43 +0530

    fix(api): update Axios instance to use cookies for token management

--- client/src/apiCalls/index.jsx
+++ client/src/apiCalls/index.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
     baseURL : 'https://mine-wallet-backend.onrender.com',
+    withCredentials : true,
     headers : {
         "authorization" : `Bearer ${localStorage.getItem('token')}`
     }
