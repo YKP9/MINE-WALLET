e0ad5c41990202e9bfbe18378be83b0cc07f91ed
YKP9 <pillaiyashwanth.9@gmail.com>
2024-10-21 15:09:25 +0530

    Testing

--- client/src/apiCalls/index.jsx
+++ client/src/apiCalls/index.jsx
@@ -3,7 +3,5 @@ import axios from 'axios'
 export const axiosInstance = axios.create({
     baseURL : 'https://mine-wallet-backend.onrender.com',
     withCredentials : true,
-    headers : {
-        "Authorization" : `Bearer ${localStorage.getItem('token')}`
-    }
+   
 })
