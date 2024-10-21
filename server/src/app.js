e38f7cc7426a9a59e5f37411d0da06f172672764
YKP9 <pillaiyashwanth.9@gmail.com>
2024-10-21 14:21:53 +0530

    CORS Option Added

--- server/src/app.js
+++ server/src/app.js
@@ -4,15 +4,15 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [process.env.CORS_ORIGIN,'https://localhost:5173'],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-   
-  })
-);
+const corsOptions = {
+  origin: [process.env.CORS_ORIGIN, 'https://localhost:5173'],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+ 
+};
+
+
 app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "16kb" }));
