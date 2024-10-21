7affde9d6aa435cb8116efa72dbac3ca80f7f2bb
YKP9 <pillaiyashwanth.9@gmail.com>
2024-10-21 12:36:19 +0530

    Axios Instance : BaseURL Added

--- client/src/apiCalls/index.jsx
+++ client/src/apiCalls/index.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
+    baseURL : 'https://mine-wallet-backend.onrender.com',
     headers : {
         "authorization" : `Bearer ${localStorage.getItem('token')}`
     }
