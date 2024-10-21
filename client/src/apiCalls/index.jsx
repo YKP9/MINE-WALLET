c109c32aa56c307bd62497eed2b113eb0881b4a8
YKP9 <pillaiyashwanth.9@gmail.com>
2024-10-21 15:58:50 +0530

    Testing

--- client/src/apiCalls/index.jsx
+++ client/src/apiCalls/index.jsx
@@ -1,14 +1,34 @@
-import axios from 'axios'
-import { useCookies } from 'react-cookie'
+// import axios from 'axios'
+// import { useCookies } from 'react-cookie'
 
-const { cookies } = useCookies()
+// const { cookies } = useCookies()
 
-export const axiosInstance = axios.create({
-    baseURL : 'https://mine-wallet-backend.onrender.com',
-    withCredentials : true,
-    headers : {
-       "Authorization" : `Bearer ${cookies.accessToken}`,
+// export const axiosInstance = axios.create({
+//     baseURL : 'https://mine-wallet-backend.onrender.com',
+//     withCredentials : true,
+//     headers : {
+//        "Authorization" : `Bearer ${cookies.accessToken}`,
+//     }
+   
+   
+// })
+import { useCookies } from 'react-cookie';
+
+// Create an Axios instance
+const axiosInstance = axios.create({
+    baseURL: 'https://mine-wallet-backend.onrender.com',
+    withCredentials: true,
+});
+
+// Interceptor to attach the token to every request
+axiosInstance.interceptors.request.use((config) => {
+    const [cookies] = useCookies(['accessToken']);
+    if (cookies.accessToken) {
+        config.headers['Authorization'] = `Bearer ${cookies.accessToken}`;
     }
-   
-   
-})
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+export default axiosInstance;
