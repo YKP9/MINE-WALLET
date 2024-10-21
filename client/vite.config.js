b155ed5b97c8ee0df775456892b18572f38969e0
YKP9 <pillaiyashwanth.9@gmail.com>
2024-10-21 12:31:40 +0530

    Update API proxy configuration in Vite for deployed backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:9000',
+      '/api': {
+        target : 'https://mine-wallet-backend.onrender.com',
+        changeOrigin : true,
+      },
     },
   },
   plugins: [react()],
