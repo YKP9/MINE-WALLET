6f14e745a90f7e47ced5b89d4916cb17c7c9a11b
YKP9 <pillaiyashwanth.9@gmail.com>
2024-10-21 15:42:04 +0530

    Testing For Token

--- client/src/apiCalls/index.jsx
+++ client/src/apiCalls/index.jsx
@@ -1,7 +1,14 @@
 import axios from 'axios'
+import { useCookies } from 'react-cookie'
+
+const { cookies } = useCookies()
 
 export const axiosInstance = axios.create({
     baseURL : 'https://mine-wallet-backend.onrender.com',
     withCredentials : true,
+    headers : {
+       "Authorization" : `Bearer ${cookies.accessToken}`,
+    }
+   
    
 })
