24fca1565a5c2027bac03107678f8c887fdb6536
YKP9 <pillaiyashwanth.9@gmail.com>
2024-10-21 15:19:05 +0530

    Testing

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -218,7 +218,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
     const options = {
       httpOnly: true,
-      secure: true,
+      secure: true || process.env.NODE_ENV === "production",
     };
 
     const { accessToken, newRefreshToken } =
