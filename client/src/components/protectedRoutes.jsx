ccf0b23ff37800180a19e83557ef62b410f949ac
YKP9 <pillaiyashwanth.9@gmail.com>
2024-10-21 12:52:06 +0530

    Import Error Resolved

--- client/src/components/protectedRoutes.jsx
+++ client/src/components/protectedRoutes.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { SetUser } from "../redux/usersSlice";
 import { HideLoading, ShowLoading } from "../redux/loadersSlice";
-import { AppLayout } from "./DefaultLayout";
+import { AppLayout } from "./defaultLayout";
 
 export function ProtectedRoute(props) {
   const [cookies] = useCookies(["token"]);
